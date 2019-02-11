579cb1c638717a0a389bf4d58aca0f98ecee5a73
amustafic <32098212+amustafic@users.noreply.github.com>
2019-02-11 17:16:51 -0500

    first commi

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+//This file holds any configuration variables we may need 
+//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
+
+
+module.exports = {
+    db: {
+      uri: 'mongodb://adnan:episkeys1@ds219055.mlab.com:19055/boot3', //place the URI of your mongo database here.
+    },
+    port: process.env.PORT || 8080
+  };
+  
+  /* Now go to the JSONtoMongo.js file and include this file as a variable named 'config' with a require() */
