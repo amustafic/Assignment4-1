0960c19db1ba8babde2060c444cd194ad087dd8b
amustafic <32098212+amustafic@users.noreply.github.com>
2019-02-11 17:27:47 -0500

    ado

--- server/config/app.js
+++ server/config/app.js
@@ -2,7 +2,7 @@ var config = require('./config'),
     mongoose = require('mongoose'),   
     express = require('./express');
 
-    app.use(CORS());
+    
 module.exports.start = function() {
   var app = express.init();
   app.listen(config.port, function() {
