16c13cd6beefecf5aad5b676a7ff194779363dbc
amustafic <32098212+amustafic@users.noreply.github.com>
2019-02-11 21:20:30 -0500

    5

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -18,7 +18,7 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.deleteListing = function(index) {
      // var indexOf = $scope.listings.indexOf(index);
       $scope.listings.splice(index, 1);
-      Listings.delete(index).then(function(response) {
+      Listings.remove(index).then(function(response) {
       }, function(error) {
         console.log('Could not delete:', error);
       });
